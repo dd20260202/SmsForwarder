7da5a5c12a001def34e9fd9737204165f73b4bb6
jjss520 <562461306@qq.com>
2026-03-26 10:49:52 +0800

    Update Ipv6Controller.kt

--- app/src/main/kotlin/cn/ppps/forwarder/server/controller/Ipv6Controller.kt
+++ app/src/main/kotlin/cn/ppps/forwarder/server/controller/Ipv6Controller.kt
@@ -1,15 +1,22 @@
 package cn.ppps.forwarder.server.controller
 
 import com.yanzhenjie.andserver.annotation.GetMapping
+import com.yanzhenjie.andserver.annotation.PostMapping // 👈 新增：引入 POST 支持
 import com.yanzhenjie.andserver.annotation.RestController
-// 👇 确保这里引入的是新包名下的 Ipv6Utils
-import cn.ppps.forwarder.utils.Ipv6Utils 
+import cn.ppps.forwarder.utils.Ipv6Utils
 
 @RestController
 class Ipv6Controller {
 
+    // 1. 支持浏览器直接访问测试 (GET 请求)
     @GetMapping("/ipv6/query")
-    fun queryIpv6(): String {
+    fun queryIpv6Get(): String {
+        return Ipv6Utils.getIPv6Address()
+    }
+
+    // 2. 支持脚本自动化调用 (POST 请求)
+    @PostMapping("/ipv6/query")
+    fun queryIpv6Post(): String {
         return Ipv6Utils.getIPv6Address()
     }
     
